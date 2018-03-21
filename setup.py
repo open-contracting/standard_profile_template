0b5aac665c2e33f00e6e1994ee71d8b630495f74
James McKinney <james@slashpoundbang.com>
2018-03-21 16:38:33 -0400

    Move extras_require into install_requires

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ jsonschema_text = ocds_sphinx_directives:jsonschema_extract
 ''',
     # The dependency trees are determined by `pipdeptree -fl`
     install_requires=[
-        # Sphinx
+        # Build
+
         'sphinx-intl==0.9.9',
           'Babel==2.3.4',
             'pytz==2016.10',
@@ -27,21 +28,21 @@ jsonschema_text = ocds_sphinx_directives:jsonschema_extract
             'Pygments==2.2.0',
             'requests==2.13.0',
             'snowballstemmer==1.2.1',
+        # 'sphinxcontrib-jsonschema',
+          'jsonpointer==1.10',
+          'jsonref==0.1',
+          # 'recommonmark',
+            'CommonMark==0.5.4',
 
-        # sphinxcontrib-jsonschema
-        'jsonpointer==1.10',
-        'jsonref==0.1',
-        'CommonMark==0.5.4',
+        # Tests
+
+        'flake8==3.3.0',
+          'mccabe==0.6.1',
+          'pycodestyle==2.3.1',
+          'pyflakes==1.5.0',
+
+        # Utils
+
+        'transifex-client',
     ],
-    extras_require={
-        'utils': [  # not pinned, not part of the build
-            'transifex-client',
-        ],
-        'test': [
-            'flake8==3.3.0',
-              'mccabe==0.6.1',
-              'pycodestyle==2.3.1',
-              'pyflakes==1.5.0',
-        ],
-    },
 )
