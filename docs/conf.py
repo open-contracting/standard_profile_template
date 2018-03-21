e75ad557f4ac782b22b5dc8e2c9614ce5bd83fe2
James McKinney <james@slashpoundbang.com>
2018-03-21 01:23:31 -0400

    Configure conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# OCDS for TODO documentation build configuration file, created by
+# Documentation build configuration file, created by
 # sphinx-quickstart on Tue Mar 20 21:53:17 2018.
 #
 # This file is execfile()d with the current directory set to its
@@ -20,7 +20,13 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import os
+import subprocess
 
+import standard_theme
+from recommonmark.parser import CommonMarkParser
+from recommonmark.transform import AutoStructify
+from sphinxcontrib.opendataservices import AutoStructifyLowPriority, translate_codelists, translate_schema
 
 # -- General configuration ------------------------------------------------
 
@@ -31,7 +37,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinxcontrib.jsonschema', 'ocds_sphinx_directives', 'sphinxcontrib.opendataservices']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -41,13 +47,16 @@ templates_path = ['_templates']
 #
 # source_suffix = ['.rst', '.md']
 source_suffix = '.md'
+source_parsers = {
+    '.md': CommonMarkParser,
+}
 
 # The master toctree document.
 master_doc = 'index'
 
 # General information about the project.
 project = 'OCDS for TODO'
-copyright = '2018, Open Contracting Partnership'
+copyright = '2018 Open Contracting Partnership'
 author = 'Open Contracting Partnership'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -83,7 +92,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'standard_theme'
+html_theme_path = [standard_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -94,64 +104,33 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['../schema', '_static']
 
 
-# -- Options for HTMLHelp output ------------------------------------------
+# -- Local configuration --------------------------------------------------
 
-# Output file base name for HTML help builder.
-htmlhelp_basename = 'OCDSforTODOdoc'
+locale_dirs = ['../locale/', os.path.join(standard_theme.get_html_theme_path(), 'locale')]
+gettext_compact = False
 
+extension_registry_git_ref = 'master'
 
-# -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'OCDSforTODO.tex', 'OCDS for TODO Documentation',
-     'Open Contracting Partnership', 'manual'),
-]
+# Compile catalogs 'codelists.po' to 'codelists.mo' and 'schema.po' to 'schema.mo', so that translate_codelists and
+# translate_schema can succeed for translations.
+subprocess.run(['pybabel', 'compile', '--use-fuzzy', '-d', '../locale', '-D', 'codelists'])
+subprocess.run(['pybabel', 'compile', '--use-fuzzy', '-d', '../locale', '-D', 'schema'])
 
 
-# -- Options for manual page output ---------------------------------------
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+        'enable_eval_rst': True
+    }, True)
+    app.add_transform(AutoStructify)
+    app.add_transform(AutoStructifyLowPriority)
 
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'ocdsfortodo', 'OCDS for TODO Documentation',
-     [author], 1)
-]
+    basedir = os.path.join(os.path.dirname((os.path.realpath(__file__))), '..')
+    localedir = os.path.join(basedir, 'locale')
 
-
-# -- Options for Texinfo output -------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'OCDSforTODO', 'OCDS for TODO Documentation',
-     author, 'OCDSforTODO', 'One line description of project.',
-     'Miscellaneous'),
-]
-
-
-
+    language = app.config.overrides.get('language', 'en')
+    translate_schema('schema', ['release-schema.json'], os.path.join(basedir, 'schema'), os.path.join(basedir, 'docs', '_static'), localedir, language)  # noqa
+    for sourcedir in ('schema', 'docs/extensions'):
+        translate_codelists('codelists', os.path.join(basedir, sourcedir, 'codelists'), os.path.join(basedir, sourcedir, 'codelists_translated', language), localedir, language)  # noqa
