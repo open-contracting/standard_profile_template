5c76ffc9ce51066217e5380dce592907fd496ee6
James McKinney <james@slashpoundbang.com>
2018-03-23 18:40:01 -0400

    Add update_txconfig target, rename some variables

--- include/config.mk
+++ include/config.mk
@@ -11,7 +11,7 @@ LANGUAGES=.en $(TRANSLATIONS)
 # Directory of documentation files to build with Sphinx.
 DOCS_DIR=docs
 # Directory of catalog files.
-CATALOGS_DIR=locale
+LOCALE_DIR=locale
 # Directory in which to build documentation files.
 BUILD_DIR=build
 # Extra build files or directories.
@@ -19,9 +19,11 @@ EXTRA_BUILD_FILES=
 # Files that are built and distributed (you may use Bash extended globbing).
 DIST_FILES=
 # Directory in which to build .pot files.
-LOCALE_DIR=$(BUILD_DIR)/locale
+POT_DIR=$(BUILD_DIR)/locale
 # Directory containing assets to copy to the build directory (no trailing slash).
 ASSETS_DIR=
+# The Transifex project name.
+TRANSIFEX_PROJECT=
 
 # Compile PO files for codelists and schema to MO files, so that translate_codelists and translate_schema succeed.
 .PHONY: compile
