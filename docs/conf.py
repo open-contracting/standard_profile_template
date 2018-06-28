70516f52795d6f05598b7206702c403758d82761
James McKinney <james@slashpoundbang.com>
2018-06-28 13:23:42 -0400

    Abbreviate the version in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ author = 'Open Contracting Partnership'
 # built documents.
 #
 # The short X.Y version.
-version = '1.0.0'
+version = '1.0'
 # The full version, including alpha/beta/rc tags.
 release = '1.0.0-rc.1'
 
