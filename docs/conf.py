24c865cc6ce99231bffd9d3b4f01e885e5597c2e
James McKinney <james@slashpoundbang.com>
2018-10-31 18:56:07 -0400

    Remove extensions

--- docs/conf.py
+++ docs/conf.py
@@ -39,11 +39,7 @@ from recommonmark.transform import AutoStructify
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinxcontrib.jsonschema',
-    'sphinxcontrib.opencontracting',
-    'sphinxcontrib.opendataservices',
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
